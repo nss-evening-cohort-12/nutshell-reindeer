a17320f33126c29ec9f1741aeee9c38e7231a3ed
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-07-16 20:24:54 -0500

    auth worked

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import utils from '../../helpers/utils';
+
+const signMeIn = () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(googleProvider);
+};
+
+const loginButton = () => {
+  const domString = '<button id="google-auth" class="btn btn-warning"><i class="fab fa-google-plus"></i> LOG ME IN!!!</button>';
+  utils.printToDom('#auth', domString);
+  $('#google-auth').click(signMeIn);
+};
+
+const logoutEvent = () => {
+  $('#navbar-logout-button').click((e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  });
+};
+
+export default { logoutEvent, loginButton };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,17 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 import 'bootstrap';
 
-console.warn('Test');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  auth.logoutEvent();
+};
+
+init();
