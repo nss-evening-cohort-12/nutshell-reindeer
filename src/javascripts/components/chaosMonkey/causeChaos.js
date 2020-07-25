2b5993c558b51c4b9640df6c7ad8119735b631a5
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-07-25 17:31:29 -0500

    wip

--- src/javascripts/components/chaosMonkey/causeChaos.js
+++ src/javascripts/components/chaosMonkey/causeChaos.js
@@ -1,4 +1,8 @@
 import utils from '../../helpers/utils';
+import './chaos.scss';
+import monkey from '../../../images/angry-monkey.png';
+import lightrays from '../../../images/light-rays.jpg';
+import splat from '../../../images/paint-splash.png';
 
 let chaosCounter = 0;
 
@@ -14,6 +18,27 @@ const decreaseChaos = () => {
   } else {
     chaosCounter -= 1;
   }
+};
+
+const angryMonkey = () => `<img src="${lightrays}" id="light-rays">
+                            <img src="${monkey}" id="angry-monkey">`;
+
+const paintSplat = () => `<img src="${splat}" id="paint-splash">
+                          <h1 id="splat-text">Oh no!<br>Amy's been kidnapped by the chaos monkey!</h1>`;
+
+const runChaos = () => {
+  console.error('test click');
+  $('body').addClass('flash');
+  setTimeout(() => {
+    $('#chaos-monkey').html(angryMonkey);
+  }, 1000);
+  setTimeout(() => {
+    $('#chaos-monkey').html(paintSplat).delay(3000).fadeOut();
+  }, 5000);
+  setTimeout(() => {
+    $('#chaos-monkey').html('');
+    $('body').removeClass('flash');
+  }, 10000);
 };
 
 export default { randomChaos, getChaos, decreaseChaos };
