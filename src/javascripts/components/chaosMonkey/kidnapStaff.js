826545dfe719e51a8e44fcc129901bb16eb098bb
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-07-27 18:57:16 -0500

    wip

--- src/javascripts/components/chaosMonkey/kidnapStaff.js
+++ src/javascripts/components/chaosMonkey/kidnapStaff.js
@@ -3,9 +3,10 @@ import utils from '../../helpers/utils';
 
 const kidnapStaff = () => {
   staffData.getStaff()
+  const kidnappableStaff = staffList.filter((actives) => actives.isActive === true);
+  const kidnappedStaff = kidnappableStaff[utils.randomNum(0, kidnappableStaff.length)];
     .then((staffList) => {
-      const kidnappableStaff = staffList.filter((actives) => actives.isActive === true);
-      const kidnappedStaff = kidnappableStaff[utils.randomNum(0, kidnappableStaff.length)];
+      console.error(kidnappableStaff);
       console.error(kidnappedStaff);
       staffData.patchStaff(kidnappedStaff.id, { isActive: false });
     })
