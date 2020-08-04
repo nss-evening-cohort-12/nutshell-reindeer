94ca51ab3b80807e287f462d270a4d5d033c42ea
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-08-04 16:29:38 -0500

    feature complete

--- src/javascripts/components/chaosMonkey/chaosLog.js
+++ src/javascripts/components/chaosMonkey/chaosLog.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import monkeyData from '../../helpers/data/monkeyData';
 import utils from '../../helpers/utils';
 import './chaosLog.scss';
@@ -8,11 +9,12 @@ const printChaosLog = () => {
       let domString = `<div id="chaosLogContainer" class="d-flex flex-row">
                           <span id="chaosIcon" class="mx-4"><i class="fas fa-biohazard"></i></span>
                             <div id="chaosItems">
+                            <h5>Chaos Monkey Log</h5>
                         `;
       logData.forEach((logEntry) => {
         domString += `<div class="card mb-2">
                         <div class="card-body">
-                          ${logEntry.message}<br>${logEntry.dateTime}
+                          ${logEntry.message}<br>${moment(logEntry.dateTime).format('MMMM Do YYYY, h:mm:ss a')}
                         </div>
                       </div>`;
       });
