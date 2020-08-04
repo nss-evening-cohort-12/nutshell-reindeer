72c1296db5be24e2f6b1a8cb95725e1659f3b8f4
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-08-04 02:37:12 -0500

    fixed console alignment issues and now differnet console view when logged in

--- src/javascripts/components/frontPageConsole/frontPageConsole.js
+++ src/javascripts/components/frontPageConsole/frontPageConsole.js
@@ -3,26 +3,38 @@ import mic from '../../../assets/images/mic.png';
 import keypad from '../../../assets/images/keypad.png';
 import speaker from '../../../assets/images/speaker.png';
 import pipe from '../../../assets/images/pipe.png';
+import lever from '../../../assets/images/lever.png';
+import guage from '../../../assets/images/guage.png';
+import transparentScreen from '../../../assets/images/transparentScreen.png';
+import loop from '../../../assets/images/loop.gif';
 import utils from '../../helpers/utils';
 import './frontPageConsole.scss';
+import auth from '../auth/auth';
 
 const printConsole = () => {
-  const domString = `<div id="frontConsoleCont">
-                    <img src="${screen}" id="screen">
-                    <img src="${mic}" id="mic">
+  let domString = '<div id="frontConsoleCont">';
+  if (auth.getUser()) {
+    domString += `<img src="${transparentScreen}" id="transparentScreen">
+                  <img src="${loop}" id="loop">`;
+  } else {
+    domString += `<img src="${screen}" id="screen">
+                  <div class="typewriter">
+                    <h1>WELCOME TO JURASSIC WORLD!</h1>
+                  </div>
+                  <div class="typewriter2">
+                    <h1>PLEASE LOG IN TO USE THE CONSOLE</h1>
+                  </div>
+                  <div class="typewriter3">
+                    <h1>></h1>
+                  </div>`;
+  }
+  domString += `<img src="${mic}" id="mic">
                     <img src="${keypad}" id="keypad">
                     <img src="${speaker}" id="speaker">
                     <img src="${pipe}" id="pipe">
                     <img src="${pipe}" id="pipe2">
-                    <div class="typewriter">
-                      <h1>WELCOME TO JURASSIC WORLD!</h1>
-                    </div>
-                    <div class="typewriter2">
-                      <h1>PLEASE LOG IN TO USE THE CONSOLE</h1>
-                    </div>
-                    <div class="typewriter3">
-                      <h1>></h1>
-                    </div>
+                    <img src="${lever}" id="lever">
+                    <img src="${guage}" id="guage">
                     </div>
                     `;
   utils.printToDom('#displayCards', domString);
