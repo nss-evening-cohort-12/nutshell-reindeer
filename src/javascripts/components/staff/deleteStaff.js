26a28f93de8a950e88dd26ec348351e38ce5a7a7
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-08-03 19:18:55 -0500

    fixed issue where delete staff did not delete corresponding schedule

--- src/javascripts/components/staff/deleteStaff.js
+++ src/javascripts/components/staff/deleteStaff.js
@@ -1,11 +1,20 @@
 import staffList from './staffList';
 import staffData from '../../helpers/data/staffData';
+import scheduleData from '../../helpers/data/scheduleData';
 
 const deleteStaff = (e) => {
   const collectionId = e.target.closest('.card').id;
   staffData.deleteStaffById(collectionId)
     .then(() => {
-      staffList.displayStaff();
+      scheduleData.getSchedule()
+        .then((schedules) => {
+          schedules.forEach((schedule) => {
+            if (schedule.staffId === collectionId) {
+              scheduleData.deleteScheduleById(schedule.id);
+            }
+          });
+          staffList.displayStaff();
+        });
     })
     .catch((err) => console.error('could not delete staff member ', err));
 };
